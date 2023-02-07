4077d04ed61131b55861f71d38d81c99860f1d85
Aehxy <79127140+ahmr-bot@users.noreply.github.com>
2023-02-07 20:13:21 +0800

    备案部分优化

--- components/Copyright.tsx
+++ components/Copyright.tsx
@@ -12,19 +12,16 @@ export default function Copyright() {
     <br />
     <MuiLink color="inherit" href="https://cfu.mefrp.com/">
     Frp 内网穿透联盟统一识别编码:AZWB66WB
-      </MuiLink><br />
+      </MuiLink>
+      <br />
       <MuiLink color="inherit" href="https://www.aehxy.com/">
   Aehxy
-      </MuiLink>策划 / 运营<MuiLink color="inherit" href="https://www.laecloud.com/">
-|莱云
-      </MuiLink>所有
-        <br />
       {typeof window !== 'undefined' && window.location.host === 'frp.mcserverx.com' ? (
         <MuiLink color="inherit" href="https://beian.miit.gov.cn/">
         蜀ICP备2022026470号-4
     </MuiLink>
       ) : null}
-
+     <br />
     </Typography>
   );
 }
